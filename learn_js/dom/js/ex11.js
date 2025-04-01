671a949a8889cefe177587162852f79758fde60b
HuyNM317 <huynm@fullstack.edu.vn>
2025-04-01 20:05:38 +0700

    update code

--- learn_js/dom/js/ex11.js
+++ learn_js/dom/js/ex11.js
@@ -25,83 +25,117 @@ for (let i = 0; i < items.length; i++) {
 }
 dotList.innerHTML = result;
 
-const handleChangeDot = (index) => {
-  Array.from(dotList.children).forEach(function (dot, indexDot) {
-    if (index === indexDot) {
-      dot.classList.add("active");
-    }
-    Array.from(dotList.children).forEach(function (dotItem, indexItem) {
-      if (index !== indexItem) {
-        dotItem.classList.remove("active");
+// const handleChangeDot = (index) => {
+//   Array.from(dotList.children).forEach(function (dot, indexDot) {
+//     if (index === indexDot) {
+//       dot.classList.add("active");
+//     }
+//     Array.from(dotList.children).forEach(function (dotItem, indexItem) {
+//       if (index !== indexItem) {
+//         dotItem.classList.remove("active");
+//       }
+//     });
+//   });
+// };
+
+// let id;
+// let indexAutoPlay = 0;
+
+// let isSecondLoop = false;
+
+// const handleAutoPlay = () => {
+//   nextBtn.click();
+//   if (!isSecondLoop) {
+//     if (indexAutoPlay === items.length - 2) {
+//       indexAutoPlay++;
+//       handleChangeDot(indexAutoPlay);
+//       indexAutoPlay = 0;
+//       isSecondLoop = true;
+//     } else {
+//       indexAutoPlay++;
+//       handleChangeDot(indexAutoPlay);
+//     }
+//   } else {
+//     if (indexAutoPlay === items.length - 1) {
+//       handleChangeDot(indexAutoPlay);
+//       indexAutoPlay = 0;
+//     } else {
+//       handleChangeDot(indexAutoPlay);
+//       indexAutoPlay++;
+//     }
+//   }
+// };
+
+// id = setInterval(handleAutoPlay, 3000);
+
+let indexAutoPlay = 0;
+
+const handleChangeDot = (control) => {
+  let flag = true;
+  Array.from(dotList.children).forEach(function (dot, index) {
+    if (dot.classList.contains("active") && flag) {
+      dot.classList.remove("active");
+      if (control === "next") {
+        dot.nextElementSibling.classList.add("active");
+      } else if (control === "prev") {
+        dot.previousElementSibling.classList.add("active");
       }
-    });
+      flag = false;
+    }
   });
 };
-
-let id;
-let index = 0;
-
-let isSecondLoop = false;
-
-id = setInterval(() => {
-  nextBtn.click();
-  if (!isSecondLoop) {
-    if (index === items.length - 2) {
-      index++;
-      handleChangeDot(index);
-      index = 0;
-      isSecondLoop = true;
-    } else {
-      index++;
-      handleChangeDot(index);
-    }
-  } else {
-    if (index === items.length - 1) {
-      handleChangeDot(index);
-      index = 0;
-    } else {
-      handleChangeDot(index);
-      index++;
-    }
-  }
-}, 3000);
 
 nextBtn.addEventListener("click", (e) => {
   if (Math.abs(position) < totalWidth - itemWidth) {
     position -= itemWidth;
     carouselInner.style.transform = `translateX(${position}px)`;
+    handleChangeDot("next");
   } else {
     carouselInner.style.transform = `translateX(0px)`;
     position = 0;
+    Array.from(dotList.children).forEach(function (dot, index) {
+      if (index === 0) {
+        dot.classList.add("active");
+      } else {
+        if (dot.classList.contains("active")) {
+          dot.classList.remove("active");
+        }
+      }
+    });
   }
 });
 
-nextBtn.addEventListener("mousedown", (e) => {
-  clearInterval(id);
-});
+// nextBtn.addEventListener("mousedown", (e) => {
+//   indexAutoPlay++;
+//   if (indexAutoPlay > items.length - 1) {
+//     indexAutoPlay = 0;
+//   }
+//   clearInterval(id);
+// });
 
-nextBtn.addEventListener("mouseup", (e) => {
-  id = setInterval(() => {
-    nextBtn.click();
-  }, 3000);
-});
+// nextBtn.addEventListener("mouseup", (e) => {
+//   id = setInterval(handleAutoPlay, 3000);
+// });
 
 prevBtn.addEventListener("click", (e) => {
   if (Math.abs(position) > 0) {
     position += itemWidth;
     carouselInner.style.transform = `translateX(${position}px)`;
+    handleChangeDot("prev");
   }
 });
 
-prevBtn.addEventListener("mousedown", (e) => {
-  clearInterval(id);
-});
+// prevBtn.addEventListener("mousedown", (e) => {
+//   indexAutoPlay--;
+//   if (indexAutoPlay < 0) {
+//     indexAutoPlay = items.length - 1;
+//   }
+//   clearInterval(id);
+// });
 
-prevBtn.addEventListener("mouseup", (e) => {
-  id = setInterval(() => {
-    nextBtn.click();
-  }, 3000);
-});
+// prevBtn.addEventListener("mouseup", (e) => {
+//   id = setInterval(handleAutoPlay, 3000);
+// });
 
 const dotItems = document.querySelectorAll(".dot-item");
 
@@ -129,3 +163,20 @@ dotItems.forEach(function (item, index) {
     }
   });
 });
+
+// dotItems.forEach(function (item, index) {
+//   item.addEventListener("mousedown", () => {
+//     if (!isSecondLoop) {
+//       if (index === items.length - 1) {
+//         indexAutoPlay = 0;
+//       }
+//     } else {
+//       indexAutoPlay = index;
+//     }
+//     clearInterval(id);
+//   });
+
+//   item.addEventListener("mouseup", () => {
+//     id = setInterval(handleAutoPlay, 3000);
+//   });
+// });
